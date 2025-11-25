2494065115a104599a870b76024f24da3964ba0d
bbbsara <noor.z.majeed@tu.edu.iq>
2025-11-26 00:01:37 +0300

    Update configuration parameters and colors

--- config.js
+++ config.js
@@ -1,14 +1,20 @@
 const CONFIG = {
-    stage1_trials: 10,       
-    stage2_time: 45,         
 
+    // رابط Google Apps Script الجديد
+    scriptURL: "https://script.google.com/macros/s/AKfycbyHiRnOEyWcZn-0WhlLojOcAcxPaypym3SCEuHPAjDVxYr3zSOEFt5RzqDaBAFtOawFjw/exec",
+
+    // عدد محاولات المرحلة الأولى
+    stage1_trials: 8,
+
+    // مدة المرحلة الثانية بالثواني
+    stage2_time: 45,
+
+    // الألوان المستخدمة في الاختبار
     colors: [
-        { name: "أحمر",  code: "red" },
-        { name: "أخضر", code: "green" },
-        { name: "أزرق",  code: "blue" },
+        { name: "أحمر", code: "red" },
+        { name: "أزرق", code: "blue" },
         { name: "أصفر", code: "yellow" },
+        { name: "أخضر", code: "green" },
         { name: "أسود", code: "black" }
-    ],
-
-    scriptURL: "https://script.google.com/macros/s/AKfycbyHiRnOEyWcZn-0WhlLojOcAcxPaypym3SCEuHPAjDVxYr3zSOEFt5RzqDaBAFtOawFjw/exec"
+    ]
 };
