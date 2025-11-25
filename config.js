dbe54726605cb0aa80804e07fd8b64a2e840ae25
bbbsara <noor.z.majeed@tu.edu.iq>
2025-11-25 23:26:02 +0300

    Refactor CONFIG object and update script URL

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
-    stage1_trials: 10, // عدد محاولات المرحلة الأولى
-    stage2_time: 45,   // زمن المرحلة الثانية بالثواني
+    stage1_trials: 10,       
+    stage2_time: 45,         
+
     colors: [
-        { name: 'أحمر', code: 'red' },
-        { name: 'أخضر', code: 'green' },
-        { name: 'أزرق', code: 'blue' },
-        { name: 'أصفر', code: 'yellow' },
-        { name: 'أسود', code: 'black' }
+        { name: "أحمر",  code: "red" },
+        { name: "أخضر", code: "green" },
+        { name: "أزرق",  code: "blue" },
+        { name: "أصفر", code: "yellow" },
+        { name: "أسود", code: "black" }
     ],
 
-    // 🔥 ضع رابط Google Apps Script هنا
-    script_url: "https://script.google.com/macros/s/AKfycbwpWIbsW9pgNk6yCEAShL5Zd-0ljz_kk3YejBOIC3bJPu7BqKTeHy01AG2yqWw_00TO/exec"
+    scriptURL: "https://script.google.com/macros/s/AKfycbyHiRnOEyWcZn-0WhlLojOcAcxPaypym3SCEuHPAjDVxYr3zSOEFt5RzqDaBAFtOawFjw/exec"
 };
